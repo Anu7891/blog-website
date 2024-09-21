72e24e4fab624f50a807f720a2893cefe70d4dff
Anubhav7890 <anubhavsingh@appscrip.co>
2024-09-21 19:44:42 +0530

    Title: Fixed Categories and Mobile UI Dev:Anubhav Date:21-09-2024 Description: Fixed Categories and Mobile UI

--- src/app/components/sideArticles/sideArticles.jsx
+++ src/app/components/sideArticles/sideArticles.jsx
@@ -7,7 +7,7 @@ const SideArticles = ({ articles }) => {
         <div className={`${Styles?.sideArticles}`}>
             {articles.map((article, index) => (
                 <div  className={`${Styles?.sideArticle}`} key={article?.catgoryId + "index" + "product" + article?.title + "randomthings "+ index}>
-                    <Link href={article?.code || ""} className={`${Styles?.sideArticleImage}`} style={{ backgroundImage: `url(${article?.image})` }}>
+                    <Link href={`/${article?.code}`} className={`${Styles?.sideArticleImage}`} style={{ backgroundImage: `url(${article?.image})` }}>
                        <div className={`${Styles?.overlayInner} gap_8`}>
                           <p className={`${Styles?.description} mb-0  md:text-base mt-0`}>{article?.title}</p>
                         </div>
