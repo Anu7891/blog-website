2c45ad3e06b47be520205973c033829c2b3f94d3
Anubhav7890 <anubhavsingh@appscrip.co>
2024-10-19 19:54:15 +0530

    Title:Fixed Some Code Dev:Anubhav Date:19-10-2024 Description: Fixed Some Code

--- src/app/components/mostRead/MostRead.jsx
+++ src/app/components/mostRead/MostRead.jsx
@@ -1,27 +1,26 @@
 "use client"
 
 import React from 'react'
-// import { SECOND_ARTICLE_IMAGE_FOUR, SECOND_ARTICLE_IMAGE_ONE, SECOND_ARTICLE_IMAGE_THREE, SECOND_ARTICLE_IMAGE_TWO } from '../../../../lib/config';
+import dynamic from 'next/dynamic';
 import Styles from "./mostRead.module.css";
-import MostReadSideArticle from '../mostReadSideArticle/mostReadSideArticle';
-import Wrapper from '../../../../hoc/wrapper';import CustomTitle from '../../../../components/title/customTitle';
+import CustomTitle from '../../../../components/title/customTitle';
+const MostReadSideArticle = dynamic(() => import('../mostReadSideArticle/mostReadSideArticle'));
 ;
 
 export const MostReadSection = ({blogs}) => {
 
-
   return (
-    <Wrapper>
-    <div className='most-read-wrapper pt-2 md:mt-4 md:pt-4 px-3 md:px-0'>
-            {/* ------------------- Title ------------------------- */}
-             <CustomTitle title={"Most Read"} />
+      <div className='most-read-wrapper pt-2 md:mt-4 md:pt-4 px-3 md:px-0'>
+          {/* ------------------- Title ------------------------- */}
+            <CustomTitle title={"Most Read"} />
 
-            {/* ------------------- Most Read Side Article ------------------------- */}
-            <div className={`${Styles?.mostReadArticleWrapper} py-6 md:p-0 md:my-6`}>
-                 <MostReadSideArticle  articles={blogs}/> 
-            </div>
-    
-        </div>
-    </Wrapper>
+          {/* ------------------- Most Read Side Article ------------------------- */}
+          {blogs && (
+          <div className={`${Styles?.mostReadArticleWrapper} py-6 md:p-0 md:my-6`}>
+                <MostReadSideArticle  articles={blogs}/> 
+          </div>
+          )}
+  
+      </div>
   )
 }
