3d226d699ede84f5e7b65745c6a51a3e4fb3d751
Anubhav Kailash Singh <100994261+Anu7891@users.noreply.github.com>
2024-11-17 23:43:47 +0530

    Title: Privacy Policy && Terms-Conditions Page New Content Date:17-11-2024 Dev:Anubhav Description:Added new content for terms and condiiton && privacy policy page.

--- src/app/terms-and-conditions/page.js
+++ src/app/terms-and-conditions/page.js
@@ -1,15 +1,12 @@
 // src/app/terms-and-conditions/page.js
-import React from 'react';
 import Wrapper from '../../../hoc/wrapper';
-import Header from '../../../components/header/header';
-import Footer from '../../../components/footer/Footer';
 import SEOHead from '../../../html/SEOHead';
 import { WEB_LINK } from '../../../lib/config';
 
 // Define metadata for the page
 export const metadata = {
     title: "Terms and Conditions | Crypto3Blog",
-    description: "Read the terms and conditions for using Crypto3Blog.",
+    description: "Review the terms and conditions for using Crypto3Blog.",
     keywords: "terms, conditions, Crypto3Blog",
     url: WEB_LINK, // Set your website URL here
 };
@@ -17,8 +14,7 @@ export const metadata = {
 const TermsAndConditions = () => {
     return (
         <Wrapper>
-
-           {/* SEO Head for managing page metadata */}
+            {/* SEO Head for managing page metadata */}
             <SEOHead
                 title={metadata?.title}
                 description={metadata?.description}
@@ -26,61 +22,53 @@ const TermsAndConditions = () => {
                 url={metadata?.url}
             />
 
-
             {/* ------------------------ Terms and Conditions Content ---------------------------- */}
             <div className="px-3 md:px-10 pt-14 mt-14 md:mt-0 pb-6 md:py-8 commonContentBackground">
-                <h1 className="text-3xl font-bold mb-4">Terms and Conditions of Use</h1>
-                <p className="mb-4">Welcome to Crypto3Blog!</p>
+                <h1 className="text-3xl font-bold mb-4">Terms and Conditions</h1>
+                <p className="mb-4">Using this website is governed by the following terms and conditions.</p>
+
+                <h2 className="text-2xl font-semibold mt-6 mb-3">1. General Information</h2>
                 <p className="mb-4">
-                    These Terms and Conditions govern your use of the Crypto3Blog website (the “Website”) and all content, products, and services provided by Crypto3Blog (collectively referred to as the “Service”). By accessing or using the Service, you agree to be bound by these Terms and Conditions. If you disagree with any part of these terms, then you may not access the Service.
+                    The information on Crypto3Blog should not be regarded as financial, legal, or investment advice.
+                    If you need professional advice, you should refer to a professional before taking any step about
+                    any piece of information contained in this website.
                 </p>
 
-                <h2 className="text-2xl font-semibold mt-6 mb-3">1. Intellectual Property</h2>
+                <h2 className="text-2xl font-semibold mt-6 mb-3">2. Use of Content</h2>
                 <p className="mb-4">
-                    The Service and its original content, features, and functionality are owned by Crypto3Blog and are protected by international copyright, trademark, patent, trade secret, and other intellectual property or proprietary rights laws.
+                    You can use the content for personal purposes only without any intention of monetary profit.
+                    Spread the word, but wherever shared, please attribute to Crypto3Blog.
                 </p>
 
-                <h2 className="text-2xl font-semibold mt-6 mb-3">2. User Accounts</h2>
+                <h2 className="text-2xl font-semibold mt-6 mb-3">3. Information Accuracy</h2>
                 <p className="mb-4">
-                    To access certain features of the Service, you may be required to create an account. You agree to provide accurate, current, and complete information when creating or updating your account. You are responsible for maintaining the confidentiality of your account and password and for restricting access to your computer or device. You agree to accept responsibility for all activities that occur under your account or password.
+                    Though we are trying to be accurate, we are human beings, and sometimes, the rates in the
+                    cryptocurrency world change with a wink of the eye. We do not assume any liability for any
+                    inaccuracies, omissions, or other decisions that stem from our content.
                 </p>
 
-                <h2 className="text-2xl font-semibold mt-6 mb-3">3. User Content</h2>
+                <h2 className="text-2xl font-semibold mt-6 mb-3">4. Intellectual Property</h2>
                 <p className="mb-4">
-                    You retain ownership of any content you submit, post, or display on or through the Service (“User Content”). However, by submitting, posting, or displaying User Content, you grant Crypto3Blog a non-exclusive, royalty-free, worldwide, irrevocable, and sub-licensable license to use, reproduce, adapt, distribute, publish, publicly perform, publicly display, and otherwise exploit such User Content.
+                    All copy of the article, pictures, and graphics are owned by Crypto3Blog. Any other use is unauthorized.
                 </p>
 
-                <h2 className="text-2xl font-semibold mt-6 mb-3">4. Prohibited Conduct</h2>
-                <ul className="list-disc ml-6 mb-4">
-                    <li>Use the Service for any illegal purpose or in violation of any local, state, national, or international law.</li>
-                    <li>Violate, or encourage others to violate, any right of a third party, including by infringing or misappropriating any third-party intellectual property right.</li>
-                    <li>Interfere with security-related features of the Service, including by disabling or circumventing features that prevent or limit use or copying of any content.</li>
-                    <li>Modify, adapt, translate, reverse engineer, decompile, or disassemble any portion of the Service.</li>
-                </ul>
-
-                <h2 className="text-2xl font-semibold mt-6 mb-3">5. Limitation of Liability</h2>
+                <h2 className="text-2xl font-semibold mt-6 mb-3">5. Disclaimer of Liability</h2>
                 <p className="mb-4">
-                    In no event shall Crypto3Blog, nor its directors, employees, partners, agents, suppliers, or affiliates, be liable for any indirect, incidental, special, consequential, or punitive damages, including without limitation, loss of profits, data, use, goodwill, or other intangible losses, resulting from:
-                </p>
-                <ul className="list-disc ml-6 mb-4">
-                    <li>your access to or use of or inability to access or use the Service.</li>
-                    <li>any conduct or content of any third party on the Service.</li>
-                    <li>any content obtained from the Service.</li>
-                </ul>
-
-                <h2 className="text-2xl font-semibold mt-6 mb-3">6. Governing Law</h2>
-                <p className="mb-4">
-                    These Terms shall be governed and construed in accordance with the laws of India, without regard to its conflict of law provisions.
+                    We disclaim any responsibility for loss, damage, or negative outcome resulting from visiting our
+                    website or any other as a result of linking from it. Activities with cryptocurrencies carry risks,
+                    and visitors shall be cautious.
                 </p>
 
-                <h2 className="text-2xl font-semibold mt-6 mb-3">7. Changes to Terms</h2>
+                <h2 className="text-2xl font-semibold mt-6 mb-3">6. Terms Changes</h2>
                 <p className="mb-4">
-                    Crypto3Blog reserves the right, at its sole discretion, to modify or replace these Terms at any time. If a revision is material, we will provide at least 30 days’ notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.
+                    Crypto3Blog has the right to alter or modify these terms at any time. Modifications of the terms
+                    appear on the page, and further usage of the website by the visitor would mean they have accepted
+                    such changes.
                 </p>
 
-                <h2 className="text-2xl font-semibold mt-6 mb-3">8. Contact Us</h2>
+                <h2 className="text-2xl font-semibold mt-6 mb-3">7. Contact Us</h2>
                 <p>
-                    If you have any questions or concerns regarding these Terms and Conditions, please contact us.
+                    All queries or information can be sent to the following email: <a href="mailto:cryptoa3new@gmail.com" className="text-blue-500 underline">cryptoa3new@gmail.com</a>.
                 </p>
             </div>
         </Wrapper>
